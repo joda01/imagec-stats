2587d44b4cdc3a9058284e29785fb182959392c1
Joachim Danmayr <joachim@danmayr.org>
2025-02-27 06:21:23 +0100

    chore: Use strptime

--- get-downloads.py
+++ get-downloads.py
@@ -179,7 +179,7 @@ def generateGraph(lastXDays):
         x_days_ago = today - datetime.timedelta(days=lastXDays)
         
         for dateTime,data in parsedData.items():
-            date_obj = datetime.strptime(dateTime, "%Y-%m-%d")
+            date_obj = strptime(dateTime, "%Y-%m-%d")
             if x_days_ago <= date_obj <= today:
                 time_values.append(dateTime)
                 y_values.append(int(data["downloadsToday"]))
